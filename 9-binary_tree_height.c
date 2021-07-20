afad068c400b365f69238530af37f55ec47dda8b
adrs condezo <ra.condezomonge@gmail.com>
2021-07-19 23:04:53 -0500

    Feat(9-binary_tree_height.c):[Add binary_tree_height function]

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - a function that measures the height of a binary tree
+ * @tree: is a pointer to the root node of the tree to measure the height.
+ *
+ * Return: 0
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t right_side = 0;
+	size_t left_side = 0;
+
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	{
+		return (0);
+	}
+
+	left_side = 1 + binary_tree_height(tree->left);
+	right_side = 1 + binary_tree_height(tree->right);
+
+	if (left_side > right_side)
+	{
+		return (left_side);
+	}
+
+	return (right_side);
+}
