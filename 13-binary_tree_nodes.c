c53e2f1bd09a061f0a84d3c915c6976fd003ca4b
adrs condezo <ra.condezomonge@gmail.com>
2021-07-19 23:25:22 -0500

    Feat(13-binary_tree_nodes.c):[Add binary_tree_nodes function]

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes -a function that counts the nodes with
+ * at least 1 child in a binary tree
+ * @tree: a pointer to the root node of the tree to count the number of nodes
+ *
+ * Return: 0 if tree is NULL otherwise 1
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if (tree->left || tree->right)
+	{
+		return (binary_tree_nodes(tree->left)
+			+ binary_tree_nodes(tree->right) + 1);
+	}
+	else
+	{
+		return (0);
+	}
+}
