a9225588cccb01f700a2f5f4386355330bc3dfd3
adrs condezo <ra.condezomonge@gmail.com>
2021-07-22 18:30:59 -0500

    Feat(17-binary_tree_is_sibling.c):[Add binary_tree_is_sibling.c]

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -2,18 +2,18 @@
 
 /**
  * binary_tree_sibling - a function that finds the sibling of a node
- * @tree: a pointer to the root node of the tree to count the number of nodes
+ * @node: is a pointer to the node to find the sibling
  *
  * Return: the node
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-        if (!node || !node->parent)
-                return (NULL);
-        if (!node->parent->left || !node->parent->right)
-                return (NULL);
+	if (!node || !node->parent)
+		return (NULL);
+	if (!node->parent->left || !node->parent->right)
+		return (NULL);
 
-        if (node->parent->left == node)
-                return (node->parent->right);
-        return(node->parent->left);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
 }
