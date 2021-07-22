313230f595f09bfc00ce88e8cde6d5bbed1754e6
adrs condezo <ra.condezomonge@gmail.com>
2021-07-22 18:22:07 -0500

    Feat(16-binary_tree_is_perfect.c):[Add binary_tree_is_perfect.c]

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,18 +8,18 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
- size_t right_side = 0;
- size_t left_side = 0;
+	size_t right_side = 0;
+	size_t left_side = 0;
 
- if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-  return (0);
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
 
- left_side = 1 + binary_tree_height(tree->left);
- right_side = 1 + binary_tree_height(tree->right);
+	left_side = 1 + binary_tree_height(tree->left);
+	right_side = 1 + binary_tree_height(tree->right);
 
- if (left_side > right_side)
-  return (left_side);
- return (right_side);
+	if (left_side > right_side)
+		return (left_side);
+	return (right_side);
 }
 
 /**
