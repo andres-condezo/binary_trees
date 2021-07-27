47930f48ed9f3a04dfda56ecafc2d2fdfa3cff5a
adrs condezo <ra.condezomonge@gmail.com>
2021-07-27 11:09:52 -0500

    Feat(100-binary_trees_ancestor.c):[Add binary_trees_ancestor file]

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_trees_ancestor
+(const binary_tree_t *first, const binary_tree_t *second)
+
+{
+	binary_tree_t *anc = (binary_tree_t *)first;
+	binary_tree_t *anc2 = (binary_tree_t *)second;
+
+	while (anc)
+	{
+		anc2 = (binary_tree_t *)second;
+		while (anc2)
+		{
+			if (anc == anc2)
+			{
+				return (anc);
+			}
+			anc2 = anc2->parent;
+		}
+		anc = anc->parent;
+	}
+
+	return (NULL);
+}
